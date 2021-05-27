d2efde0b4da11318b86b7131905a2f1a72c4044d
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2021-05-27 12:38:26 +0800

    first commit

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    public float timeToSwitch = 5.0f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        timeToSwitch -= Time.deltaTime;
+
+        if (timeToSwitch <= 0.0f)
+        {
+            GoToMainScene();
+        }
+    }
+
+    void GoToMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
