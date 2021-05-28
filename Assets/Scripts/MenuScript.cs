668a8e6e68e2379a92ba13798f1ba0bfcb46488d
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2021-05-28 13:30:02 +0800

    add scrolling bg

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public void StartTheGame()
     {
-
+        SceneManager.LoadScene("GameScene");
     }
 
     public void EndTheGame()
