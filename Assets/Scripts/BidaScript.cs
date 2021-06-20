6364fe73a7080095df8754518001e944ecf9374a
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2021-06-20 08:00:19 +0800

    jump

--- Assets/Scripts/BidaScript.cs
+++ Assets/Scripts/BidaScript.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class BidaScript : MonoBehaviour
 {
     private Animator thisAnimator;
+    private Rigidbody2D rb;
+    public AudioSource BarkSound;
+    private float previousAnimSpeed;
+    private bool canJump = true;
     void Start()
     {
         thisAnimator = this.GetComponent<Animator>();
-
+        previousAnimSpeed = thisAnimator.speed;
+        rb = this.GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreCollision(GameObject.Find("close").GetComponent<Collider2D>(), this.GetComponent<Collider2D>());
 
         if (GlobalVariableScript.SelectedCharacter == 0)
         {
@@ -17,6 +23,26 @@ public class BidaScript : MonoBehaviour
         {
             thisAnimator.runtimeAnimatorController = Resources.Load("2_1") as RuntimeAnimatorController;
         }
+    }
 
+    private void OnMouseDown()
+    {
+        if (canJump)
+        {
+            float jumpVelocity = 23.5f;
+            rb.velocity = Vector2.up * jumpVelocity;
+            BarkSound.Play();
+            thisAnimator.speed = 0;
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "INVI_PLATFORM")
+        {
+            thisAnimator.speed = previousAnimSpeed;
+            canJump = true;
+        }
     }
 }
